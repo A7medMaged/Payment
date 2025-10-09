3e651f7d1c28ae74bd0f8d663609773c7cc4cd6b
AhmedMaged1722 <am9351705@gmail.com>
2025-10-09 23:22:32 +0300

    Add dartz dependency and implement CheckoutRepo with payment processing logic

---
+++ lib/features/checkout/data/repos/checkout_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment/core/errors/failure.dart';
+import 'package:payment/features/checkout/data/models/payment_intent_request.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> makePayment({
+    required PayMentIntentRequest paymentIntentRequest,
+  });
+}
---
+++ lib/features/checkout/data/repos/checkout_repo_impl.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment/core/errors/failure.dart';
+import 'package:payment/core/utils/stripe_service.dart';
+import 'package:payment/features/checkout/data/models/payment_intent_request.dart';
+import 'package:payment/features/checkout/data/repos/checkout_repo.dart';
+
+class CheckoutRepoImpl extends CheckoutRepo {
+  final StripeService stripeService = StripeService();
+
+  @override
+  Future<Either<Failure, void>> makePayment({
+    required PayMentIntentRequest paymentIntentRequest,
+  }) async {
+    try {
+      await stripeService.makePayment(
+        paymentIntentRequest: paymentIntentRequest,
+      );
+      return const Right(null);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
